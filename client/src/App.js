766b023b3e035a26bf63933140b7895bd498d92f
Pierre-Paul <pierrepaul.g@outlook.com>
2019-02-06 11:27:02 +0100

    Finition structure connexion

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import ConnexionPage from './components/pages/ConnexionPage.jsx';
+import ConnectionPage from './components/pages/ConnectionPage.jsx';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <ConnexionPage />
+        <ConnectionPage />
       </div>
     );
   }
