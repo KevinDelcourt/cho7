c12919872767464fde1f211432f18f09582af4d3
Pierre-Paul <pierrepaul.g@outlook.com>
2019-02-11 17:25:15 +0100

    Creation composant header template

---
+++ client/src/components/organisms/HeaderTemplate.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class HeaderTemplate extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="banniere" />
+                <div className="navBar" />
+            </div>
+        );
+    }
+}
+
+export default HeaderTemplate;
