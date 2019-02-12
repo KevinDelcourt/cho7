857546a52446e3471b2b46e8be9cacc4c257a9c4
KevinDelcourt <delcokevgmail.com>
2019-02-12 10:01:48 +0100

    Modif Footer + Création de scripts à la racine du dossier

--- client/src/components/organisms/Footer.jsx
+++ client/src/components/organisms/Footer.jsx
@@ -15,13 +15,11 @@ const FooterDiv = styled.div`
     line-height: normal;
     text-align: center;
 
-
     background: rgba(213, 191, 159, 0.67);
-    border-radius: 20px;
+    border-radius: 10px;
 `;
 
-const RightLinkFooter = styled.div`
-
+const RightDiv = styled.div`
     position: absolute;
     left: 55.81%;
     right: 24.41%;
@@ -29,22 +27,20 @@ const RightLinkFooter = styled.div`
     bottom: 24.79%;
 `;
 
-const LeftLinkFooter = styled.div`
+const LeftDiv = styled.div`
     position: absolute;
     left: 13.23%;
     right: 46.94%;
     top: 24.79%;
     bottom: 24.79%;
-
 `;
 
 export default class Footer extends Component{
-
     render(){
         return(
             <FooterDiv>
-                <LeftLinkFooter><a href="#">La Compagnie de L'aventure</a></LeftLinkFooter>
-                <RightLinkFooter><a href="#">A Propos</a></RightLinkFooter>
+                <LeftDiv>{this.props.left}</LeftDiv>
+                <RightDiv>{this.props.right}</RightDiv>
             </FooterDiv>
         )
     }
--- client/src/stories/index.js
+++ client/src/stories/index.js
@@ -1,28 +1,10 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
-import { Button, Welcome } from '@storybook/react/demo';
 
-import Logo from '../components/atoms/Logo';
-import SiteTitle from '../components/atoms/SiteTitle';
 import Footer from '../components/organisms/Footer';
-
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
-storiesOf('Logo', module).add('Avec une image', () => <Logo />);
-
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>));
 
 storiesOf('organisms/Footer', module)
   .add('Footer', () => (
-      <Footer />
+      <Footer left="gaute" right="droiche"/>
   )
 );
