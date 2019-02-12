68bfed883fe324a1419d9dc3ac8bb246b2e5d524
Natacha <natika.orsas@gmail.com>
2019-02-12 19:55:24 +0100

    clean

--- client/src/components/pages/AccueilPage.jsx
+++ client/src/components/pages/AccueilPage.jsx
@@ -5,7 +5,7 @@ import HeaderTemplate from '../organisms/HeaderTemplate';
 const AccueilPage = () => (
     <div>
       <HeaderTemplate />
-      <h2>Accueil</h2>
+      <h2 class="App-link">Accueil</h2>
       <FooterTemplate left={<a href="/">La Compagnie de l'Aventure</a>} right={<a href="/">A propos</a>}/>
     </div>
   )
