da078c956c6d0c47e13fba328048787554a789ca
sahla <medelciss@gmail.com>
2019-02-12 10:39:25 +0100

    creer composant banniere

---
+++ client/src/components/organisms/HeaderTemplate.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class HeaderTemplate extends React.Component {
+    render(){
+        return(
+        <div>
+            <div className ="banniere">
+                <Banniere/>               
+                
+            </div>
+            
+            <div className ="navBar"/>       
+        </div>
+        );
+        }
+}
+export default HeaderTemplate;
+    
