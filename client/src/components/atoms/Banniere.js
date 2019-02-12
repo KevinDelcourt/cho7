4936b390e87b4f03a6d875dd21b5ec12e668a655
sahla <medelciss@gmail.com>
2019-02-12 10:41:16 +0100

    fix banniere

---
+++ client/src/components/atoms/Banniere.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Banniere = styled.img`
+    width: 150px;
+    height: 150px;
+    display: flex;
+`;
+
+export default Banniere;
--- client/src/components/organisms/HeaderTemplate.jsx
+++ client/src/components/organisms/HeaderTemplate.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import banniere from './../../assets/images/banniere.png';
 
 class HeaderTemplate extends React.Component {
     render(){
         return(
         <div>
             <div className ="banniere">
-                <Banniere/>               
+                <Banniere src={banniere} alt="banniere"/>               
                 
             </div>
             
