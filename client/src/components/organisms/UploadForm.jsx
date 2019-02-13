cbb446c29a3ad9e783779dbdd4cef204780bfb9f
Pierre-Paul <pierrepaul.g@outlook.com>
2019-02-13 10:26:24 +0100

    feat: select file

--- client/src/components/organisms/UploadForm.jsx
+++ client/src/components/organisms/UploadForm.jsx
@@ -1,42 +1,45 @@
 import styled from "styled-components";
 import React from 'react';
-import ConnexionButton from "../atoms/ConnectionButton";
 
 const PlacementNCreation = styled.div`
-    position: absolute;
-    left: 0.85%;
-    right: 61.84%;
-    top: 1.64%;
-    bottom: 85.77%;
+	position: absolute;
+	left: 0.85%;
+	right: 61.84%;
+	top: 1.64%;
+	bottom: 85.77%;
 
-    font-family: Almendra SC;
-    font-style: normal;
-    font-weight: normal;
-    line-height: normal;
-    font-size: 30px;
-    text-align: center;    
+	font-family: Almendra SC;
+	font-style: normal;
+	font-weight: normal;
+	line-height: normal;
+	font-size: 30px;
+	text-align: center;    
 `
 
 const UploadContainer = styled.div`
-    margin-top: 8vh;
-    height: 15vh;
+	margin-top: 8vh;
+	height: 15vh;
 `;
 
 const FooterUpload = styled.div`
-    position: absolute;
-    right: 2.12%;
-    bottom: 3.01%;
-    
-    padding:5px;
+	position: absolute;
+	right: 2.12%;
+	bottom: 3.01%;
+	
+	padding:5px;
 `;
 
 export default class UploadForm extends React.Component{
 
-    render(){
-        return(<div>
-            <PlacementNCreation children="Nouvelle Création" />
-            <UploadContainer children="Formulaire Upload ici" />
-            <FooterUpload children="Bouton publier"/>
-        </div>)
-    }
+	render() {
+		return(
+			<div>
+				<PlacementNCreation children="Nouvelle Création" />
+				<UploadContainer>
+					<input type="file" name="creation" accept="audio/mp3, audio/wav" />
+				</UploadContainer>
+				<FooterUpload children="Bouton publier"/>
+			</div>
+		)
+	}
 }
