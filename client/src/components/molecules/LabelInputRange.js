5a74b95d8e487e0e1e9a9dc73aa33d2787a207ec
Carlos <carlos973junior@gmail.com>
2019-03-13 09:37:06 +0100

    ajout des composant des etats

---
+++ client/src/components/molecules/LabelInputRange.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import LabelBase from '../atoms/LabelBase';
+
+class LabelInputRange extends React.Component {
+    render(){
+        return(
+            <div>
+                <LabelBase children={this.props.label} />
+                <input name={"libelle["+this.props.index+"]"} value={this.props.label} style={"display: hidden"} />
+                <input type='range' min="0" max="100" step="1" name={"valeur["+this.props.index+"]"} />
+            </div>
+        );
+    }
+}
+
+export default LabelInputRange;
---
+++ client/src/components/organisms/InputAddButton.js
@@ -0,0 +1,35 @@
+import React from "react";
+import AddButton from "../atoms/AddButton";
+import InputBase from "../atoms/InputBase";
+import LabelInputRange from "../molecules/LabelInputRange";
+
+class InputAddButton extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            nameEtat: []
+        };
+        this.getStateName = this.getStateName.bind(this);
+    }
+
+    getStateName(){
+        let nameState = document.getElementById("inputStateName").value;
+        let tab = this.state.nameEtat;
+        tab.push(nameState);
+        this.setState({nameEtat: tab});
+    }
+
+    render(){
+        return(
+            <div>
+                <InputBase id="inputStateName"/>
+                <AddButton type="button" value="+" onClick={this.getStateName} />
+                <div>
+                    {this.state.nameEtat.map((name, index) => (<LabelInputRange label={name+": "} index={index}/> ))}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default InputAddButton;
