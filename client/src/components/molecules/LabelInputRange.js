6d2ce3c6cb04911d22e6647e0d4a97cd37c8f9af
Natacha <natika.orsas@gmail.com>
2019-03-23 13:30:25 +0100

    fix : label instead labelbase

--- client/src/components/molecules/LabelInputRange.js
+++ client/src/components/molecules/LabelInputRange.js
@@ -10,7 +10,7 @@ class LabelInputRange extends React.Component {
 	render(){
 		return(
 			<styleDivContainer>
-				<LabelBase children={this.props.label} />
+				<Label children={this.props.label} />
 				<input type="hidden" name={"libelle["+this.props.index+"]"} value={this.props.label} />
 				<input type="hidden" name={"idEtat["+this.props.index+"]"} value={this.props.idEtat} />
 				<input type='range' min="0" max="100" step="1" name={"valeur["+this.props.index+"]"} defaultValue={this.props.value}/>
