2d57b43660b1963fb49776d597c776f997e05602
Pierre-Paul <pierrepaul.g@outlook.com>
2019-04-08 15:38:17 +0200

    feat: modification et suppression projet

--- client/src/components/organisms/Projet.jsx
+++ client/src/components/organisms/Projet.jsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import React from 'react';
 import { getAvancement } from '../../modules/api';
+import { Link, Redirect } from "react-router-dom"
+import { hasRole, deleteCreation } from "../../modules/api"
 import MainContainer from './../molecules/MainContainer';
 
 const SubContainer = styled.div`
@@ -19,15 +21,29 @@ const DescriptionContainer = styled.div`
 `
 
 const StateContainer = styled.div`
-    font-family: "Ruluko", Arial, Sans-serif;
     font-size: 20px;
 `
 
+const DetailsContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+
 class Projet extends React.Component {
-    state = { avancement: [] }
+    state = { 
+        auth: false,
+        avancement: []
+    }
 
     async componentDidMount() {
+        this.setState({ auth: await hasRole("CREATEUR") })
         this.setState({ avancement: await getAvancement() })
+    }
+
+    handleDeleteClick = async (id) => {
+        if (await deleteCreation(id)) {
+            this.setState({ redirect: <Redirect to="/accueil" /> })
+        } else this.props.msgAction("Erreur dans la suppression")
     }
 
     datetostring = timestamp => {
@@ -40,21 +56,30 @@ class Projet extends React.Component {
             <MainContainer title="Projets en cours">
                 <SubContainer>
                     {this.state.avancement.map((c, index) => (
-                        <MainContainer key={index}>
-                            <h2>{c[0].titre}</h2>
+                        <MainContainer key={index} title={c[0].titre}>
                             {c.map((etat, index) => (
                                 <StateContainer key={index}>
-                                    <label>{etat.libelle}</label>
+                                    <label>{etat.libelle + ' : '}</label>
                                     {etat.valeuravancement + "%"}
                                 </StateContainer>
                             ))}
+
                             <DescriptionContainer>
                                 {c[0].description}
                             </DescriptionContainer>
-                            {this.datetostring(c[0].miseajour)}
+                            
+                            <DetailsContainer>
+                                {this.datetostring(c[0].miseajour)}
+                                
+                                <div>
+                                    <Link className="fas fa-edit" to={"/updateCreation/" + c[0].id} />
+                                    <button className="far fa-times-circle fa-2x deleteButton" onClick={() => this.handleDeleteClick(c[0].id)} />
+                                </div>
+                            </DetailsContainer>
                         </MainContainer>
                     ))}
                 </SubContainer>
+                {this.state.redirect}
             </MainContainer>
         )
     }
